751c5e084737398405603e93c6703c9140e894f6
Kanstantsisn Zvankoich <kostya.zvankovich@gmail.com>
2018-11-03 23:35:22 +0100

    change version of since, add additional check to verify filePath type

--- src/fs/isArchivePath.js
+++ src/fs/isArchivePath.js
@@ -4,7 +4,7 @@ import endsWith from '../data/endsWith'
  * Checks if `filePath` is classified as an `Archive`.
  *
  * @function
- * @since 0.0.16
+ * @since 0.0.17
  * @category fs
  * @param {*} filePath The value to check.
  * @returns {boolean} Returns `true` if `filePath` is an archive, else `false`.
@@ -12,15 +12,22 @@ import endsWith from '../data/endsWith'
  *
  * isArchivePath('ok.zip') // => true
  *
- * isArray('./code') // => false
+ * isArchivePath('./code') // => false
  */
-const isArchivePath = (filePath) =>
-  endsWith('.zip', filePath) ||
-  endsWith('.jar', filePath) ||
-  endsWith('.tar', filePath) ||
-  endsWith('.rar', filePath) ||
-  endsWith('.gz', filePath) ||
-  endsWith('.bz2', filePath) ||
-  endsWith('.7z', filePath)
+const isArchivePath = (filePath) => {
+  if (typeof filePath !== 'string') {
+    return false
+  }
+
+  return (
+    endsWith('.zip', filePath) ||
+    endsWith('.jar', filePath) ||
+    endsWith('.tar', filePath) ||
+    endsWith('.rar', filePath) ||
+    endsWith('.gz', filePath) ||
+    endsWith('.bz2', filePath) ||
+    endsWith('.7z', filePath)
+  )
+}
 
 export default isArchivePath
--- src/fs/isArchivePath.test.js
+++ src/fs/isArchivePath.test.js
@@ -12,6 +12,8 @@ describe('isArchivePath', () => {
   })
 
   test('returns false for all other values', () => {
+    expect(isArchivePath(null)).toBe(false)
+    expect(isArchivePath(undefined)).toBe(false)
     expect(isArchivePath('./code')).toBe(false)
     expect(isArchivePath('file.js')).toBe(false)
   })
