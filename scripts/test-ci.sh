dd5bb8e731b70fd40f791f2eaf56a59488e75749
Brian Neisler <me@brianneisler.com>
2018-11-07 17:06:25 -0800

    Ensure that tests in windows are run using cmd.exe so that we still test the path issues.

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -1,3 +1,9 @@
 set -e
-jest . --runInBand --forceExit --colors
+if [ $TRAVIS_OS_NAME = "windows" ]; then
+   echo "running tests in cmd.exe..."
+   cmd.exe /c jest . --runInBand --forceExit --colors
+else
+   echo "running tests in bash..."
+   jest . --runInBand --forceExit --colors
+fi
 codecov
