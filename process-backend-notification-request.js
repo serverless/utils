bfe1dc5a2dbaec1bbd312fe0d309a15f6bc18941
Mariusz Nowak <medyk@medikoo.com>
2020-06-12 14:21:33 +0200

    feat: `processBackendNotificationRequest` util

---
+++ process-backend-notification-request.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const isPlainObject = require('type/plain-object/is');
+const coerceNaturalNumber = require('type/natural-number/coerce');
+const coerceTimeValue = require('type/time-value/coerce');
+const toShortString = require('type/lib/to-short-string');
+const configUtils = require('./config');
+
+const configPropertyName = 'shownNotificationsHistory';
+
+const logError = (message) => {
+  if (!process.env.SLS_ANALYTICS_DEBUG) return;
+  process.stdout.write(`Notifications error: ${message}\n`);
+};
+
+module.exports = (notifications) => {
+  if (!Array.isArray(notifications)) {
+    logError(`Expected array, got ${toShortString(notifications)}`);
+    return null;
+  }
+  const shownNotificationsHistory = configUtils.get(configPropertyName) || {};
+  return (
+    notifications.find((notification, index) => {
+      if (!isPlainObject(notification)) {
+        logError(`Expected plain object at [${index}] got ${toShortString(notification)}`);
+        return false;
+      }
+      if (!notification.code || typeof notification.code !== 'string') {
+        logError(`Expected string at [${index}].code got ${toShortString(notification.code)}`);
+        return false;
+      }
+      if (!notification.message || typeof notification.message !== 'string') {
+        logError(
+          `Expected string at [${index}].message got ${toShortString(notification.message)}`
+        );
+        return false;
+      }
+      const lastShown = coerceTimeValue(shownNotificationsHistory[notification.code]);
+      if (lastShown) {
+        if (
+          lastShown >
+          Date.now() - (coerceNaturalNumber(notification.visibilityInterval) || 24) * 60 * 60 * 1000
+        ) {
+          return false;
+        }
+      }
+      shownNotificationsHistory[notification.code] = Date.now();
+      configUtils.set(configPropertyName, shownNotificationsHistory);
+      return true;
+    }) || null
+  );
+};
