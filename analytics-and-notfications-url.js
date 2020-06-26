c271b3f0a0e0901a3c45d5726c3e32b54a5a36ba
Mariusz Nowak <medyk@medikoo.com>
2020-06-26 10:38:35 +0200

    feat: Configure AWS metrics URL

--- analytics-and-notfications-url.js
+++ analytics-and-notfications-url.js
@@ -14,4 +14,4 @@ const isInChina = require('./is-in-china');
 
 module.exports = isInChina
   ? 'https://service-9p6tdp4y-1300963013.gz.apigw.tencentcs.com/release/'
-  : null;
+  : 'https://sp-notifications-and-metrics-v1.serverless-platform.com';
