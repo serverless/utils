069844abf6fef7f16e5af6c00bc553ccd481328d
Piotr Grzesik <pj.grzesik@gmail.com>
2021-01-26 14:44:12 +0100

    chore: Release v3.0.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ module.exports = {
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [2, 'always'],
     'header-max-length': [2, 'always', 72],
-    'scope-enum': [2, 'always', ['']],
+    'scope-enum': [2, 'always', ['', 'Config']],
     'subject-case': [2, 'always', 'sentence-case'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
