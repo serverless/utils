bce5b5c7e57bd19d209c01cc333e63b6fe2831b2
mzong <meng@fastmail.com>
2021-04-01 15:56:01 +0800

    feat: Update china notify url for dev env

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+## [4.0.1](https://github.com/serverless/utils/compare/v4.0.0...v4.0.1) (2021-04-01)
+
+### Features
+
+- Add a dev env metrics url for China user
+
 ## [4.0.0](https://github.com/serverless/utils/compare/v3.1.0...v4.0.0) (2021-03-11)
 
 ### ⚠ BREAKING CHANGES
--- analytics-and-notfications-url.js
+++ analytics-and-notfications-url.js
@@ -7,6 +7,11 @@ if (process.env.SLS_ANALYTICS_URL) {
 
 const isInChina = require('./is-in-china');
 
+const ChinaNotifyUrl =
+  process.env.SERVERLESS_PLATFORM_STAGE === 'dev'
+    ? 'https://service-pwww3r9f-1300963013.sh.apigw.tencentcs.com/release/'
+    : 'https://service-9p6tdp4y-1300963013.gz.apigw.tencentcs.com/release/';
+
 module.exports = isInChina
-  ? 'https://service-9p6tdp4y-1300963013.gz.apigw.tencentcs.com/release/'
+  ? ChinaNotifyUrl
   : 'https://sp-notifications-and-metrics-v1.serverless-platform.com';
