2d51179edf539f6a39e8e161efcd53f78769e843
Mariusz Nowak <medyk@medikoo.com>
2021-09-02 17:31:31 +0200

    feat(Log): Support hiding legacy logs

--- log-reporters/node.js
+++ log-reporters/node.js
@@ -1,6 +1,13 @@
 'use strict';
 
+const log = require('../log');
+
 const logMode = isFinite(process.env.SLS_DEV_LOG_MODE) ? Number(process.env.SLS_DEV_LOG_MODE) : 0;
+
+if (logMode & 2) {
+  // Hide legacy logs
+  log.legacy.write = () => {};
+}
 
 if (!(logMode & 1)) return;
 
