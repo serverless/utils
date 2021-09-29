928fa50f3e0cc6f21453295d2596398b456b0bfd
Mariusz Nowak <medyk@medikoo.com>
2021-09-29 18:51:08 +0200

    feat(Log): Modern logs for interactive exchange

--- inquirer/index.js
+++ inquirer/index.js
@@ -2,12 +2,18 @@
 
 'use strict';
 
+const identity = require('ext/function/identity');
 const { dirname } = require('path');
 const requireUncached = require('ncjsm/require-uncached');
 const resolve = require('ncjsm/resolve/sync');
 const chalk = require('chalk');
+const { style } = require('../log');
 
 const inquirersChalkPath = resolve(dirname(require.resolve('inquirer')), 'chalk').realPath;
+
+// With inquirer we cannot independently configure two different logging styles, therefore we
+// decide on style basing on env var
+const isModernStyle = Number(process.env.SLS_DEV_LOG_MODE) & 1;
 
 module.exports = requireUncached(inquirersChalkPath, () => {
   // Ensure distinct chalk instance for inquirer and hack it with altered styles
@@ -19,7 +25,7 @@ module.exports = requireUncached(inquirersChalkPath, () => {
     },
     bold: {
       get() {
-        return chalk.bold.yellow;
+        return isModernStyle ? identity : chalk.bold.yellow;
       },
     },
   });
@@ -28,7 +34,7 @@ module.exports = requireUncached(inquirersChalkPath, () => {
   const originalGetQuestion = BasePrompt.prototype.getQuestion;
   BasePrompt.prototype.getQuestion = function () {
     // Here we want to override the default prefix which is equal to `chalk.green('?')`
-    this.opt.prefix = '';
+    this.opt.prefix = isModernStyle ? style.strong('?') : '';
     return originalGetQuestion.call(this);
   };
 
