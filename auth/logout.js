fa05f407a5b94395cdf54d47f1ea592462717436
Mariusz Nowak <medyk@medikoo.com>
2022-05-06 15:58:38 +0200

    refactor(Auth): Refactor utils to be CLI agnostic

--- auth/logout.js
+++ auth/logout.js
@@ -1,16 +1,10 @@
 'use strict';
 
-const log = require('../log').log.get('auth');
 const configUtils = require('../config');
 
 module.exports = () => {
-  if (!configUtils.get('auth.refreshToken')) {
-    log.notice.skip('You are already logged out');
-    return false;
-  }
-  configUtils.delete('auth');
-  log.notice('Logging into the Serverless Console via the browser');
+  if (!configUtils.get('auth.refreshToken')) return false;
 
-  log.notice.success('You are now logged out of the Serverless Console');
+  configUtils.delete('auth');
   return true;
 };
