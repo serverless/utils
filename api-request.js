ea41bf4845c0fe9a050a75390c8b48ea8f7059b6
Mariusz Nowak <medyk@medikoo.com>
2022-05-11 17:52:27 +0200

    refactor(Auth): Ensure to send required header with org token

--- api-request.js
+++ api-request.js
@@ -6,16 +6,19 @@ const log = require('./log').log.get('auth');
 const ServerlessError = require('./serverless-error');
 const backendUrl = require('./lib/auth/urls').backend;
 const resolveAuthToken = require('./auth/resolve-token');
+const resolveAuthMethod = require('./auth/resolve-mode');
 
 module.exports = async (pathname) => {
   pathname = ensureString(pathname, { name: 'pathname' });
+  const authMethod = await resolveAuthMethod();
+  if (!authMethod) throw new Error('Not authenticated to send request to the Console server');
   const response = await (async () => {
     try {
+      const headers = { Authorization: `Bearer ${await resolveAuthToken()}` };
+      if (authMethod === 'org') headers['sls-token-type'] = 'orgToken';
       return await fetch(`${backendUrl}/api/identity${pathname}`, {
         method: 'GET',
-        headers: {
-          Authorization: `Bearer ${await resolveAuthToken()}`,
-        },
+        headers,
       });
     } catch (error) {
       log.debug('Server unavailable', error);
