3aa8c087d17963d4b828fdbe33ba3fda87a98c18
Dan Jarvis <Danwakeem@users.noreply.github.com>
2023-07-12 14:44:44 -0500

    fix: Set dashboardToken when using dashboard token (#218)
    * fix: Set dashboardToken when using dashboard token
    
    * refactor: Set authMethod as dashboard
    
    * refactor: Add authMethod option

--- api-request.js
+++ api-request.js
@@ -17,7 +17,7 @@ module.exports = async (pathname, options = {}) => {
   if (!isObject(options)) options = {};
   const method = ensureString(options.method, { name: 'options.method', default: 'GET' });
   const body = ensureObject(options.body, { name: 'options.body', isOptional: true });
-  const authMethod = options.noAuth ? 'none' : await resolveAuthMethod();
+  const authMethod = options.noAuth ? 'none' : options.authMethod || (await resolveAuthMethod());
   if (!authMethod) throw new Error('Not authenticated to send request to the Console server');
   const requestId = ++requestIdCounter;
   let authorization = {};
@@ -34,6 +34,7 @@ module.exports = async (pathname, options = {}) => {
       'Content-Type': 'application/json',
     };
     if (authMethod === 'org') headers['sls-token-type'] = 'orgToken';
+    if (authMethod === 'dashboard') headers['sls-token-type'] = 'dashboardToken';
     const fetchOptions = {
       method,
       headers,
